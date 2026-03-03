2375f7df5c6c8940a11c2729822ca09ecc9d92c3
Nickrockerz <d.nickrockerz060@gmail.com>
2026-03-04 03:22:40 +0530

    feat(deposite):Update deposite page

--- src/routes/depositRoutes.js
+++ src/routes/depositRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { submitDeposit } from "../controllers/deposit.controller.js";
-// import { verifyToken } from "../middleware/auth.js";
+import authenticate from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/submit",  submitDeposit);
+router.post("/submit", authenticate, submitDeposit);
 
 export default router;
